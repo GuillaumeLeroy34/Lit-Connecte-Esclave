6b6ae222c3189c342d54790182bcc4e9423a22aa
guillaumeLeroy <guillaume.leroy34@gmail.com>
2024-02-12 08:22:22 +0100

    commit initial

---
+++ src/main.cpp
@@ -0,0 +1,85 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal.h>
+// put function declarations here:
+// int b2 = 2;
+// int b3 = 3;
+int b4 = 4; // TODO buzzer  vérifier si on peut ?
+int b5 = 5; // que des boutons
+int b6 = 6;
+int b7 = 7;
+int b8 = 8;
+int b9 = 9;
+int b10 = 10;
+int b11 = 11;
+int b12 = 12;
+int b13 = 13;
+boolean changement = false;
+boolean lastState = false;
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+//? A0: Potentiomètre
+
+
+void estHumide()
+{
+ //? avec false = sec et true = humide
+
+  int valeurHumi = analogRead(A0);
+
+  if (valeurHumi > 500)
+  {
+    changement = true;
+     if(lastState != changement){
+   //   Serial.println("changement vers humide");
+   lcd.clear();
+    lcd.print(" Humide");
+  }
+
+  }
+  else
+  {
+    changement = false;
+     if(lastState != changement){
+    //  Serial.println("changement vers sec");
+    lcd.clear();
+    lcd.print(" Sec");
+  }
+
+  }
+  lastState = changement;
+ 
+}
+
+void setup()
+{
+  
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.clear();
+  //! inutiles pinMode(b2, INPUT);
+  //! inutiles pinMode(b3, INPUT);
+  // pinMode(b4, OUTPUT); //? buzzer
+  // pinMode(b5, INPUT);
+  // pinMode(b6, INPUT);
+  // pinMode(b7, INPUT);
+  // pinMode(b8, INPUT);
+  // pinMode(b9, INPUT);
+  // pinMode(b10, INPUT);
+  // pinMode(b11, INPUT);
+  // pinMode(b12, INPUT);
+  // pinMode(b13, INPUT);
+  pinMode(A2, OUTPUT);
+  analogWrite(A2, 100);
+  pinMode(A0, INPUT); //? il s'agit du potentiomètre, ou du capteur d'humidité si on change
+}
+
+void loop()
+{
+  delay(50);   // TODO a changer plus tard, c'est pour s'assurer que le micro controleur tourne pas en boucle pour rine
+               // put your main code here, to run repeatedly:
+               // estSurBord(); //
+  estHumide(); 
+}
